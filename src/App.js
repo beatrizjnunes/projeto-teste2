fe56b8797d119b705ae62a9eccfb50290b205300
beatrizjnunes <beatrizjnunes@gmail.com>
2025-08-06 15:37:02 +0100

    alteracao 3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Olá, React! Teste Dancing Queen</h1>
+      <h1>Olá, React! Teste Dancing Queen 5555555</h1>
       <p>Projeto React do zero 🎉</p>
     </div>
   );
