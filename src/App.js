73284b69a993d00ce9671dc4742a5d567b6ae483
beatrizjnunes <beatrizjnunes@gmail.com>
2025-08-06 13:10:55 +0100

    mudei o titulo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Olá, React! Teste Ivo 44</h1>
+      <h1>Olá, React! Teste Ivo 60</h1>
       <p>Este é o meu primeiro projeto React do zero 🎉</p>
     </div>
   );
