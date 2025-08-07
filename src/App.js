c1cfbd9c4e78666944034195fb09ff1939b33d8d
beatrizjnunes <beatrizjnunes@gmail.com>
2025-08-07 16:44:59 +0100

    reuniao teste

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Olá, MIguel Beatriz! Teste Dancing Queen Reuniao</h1>
+      <h1>Olá, MIguel Beatriz! Teste Dancing Queen Reuniao teste</h1>
       <p>Projeto React do zero 🎉</p>
     </div>
   );
